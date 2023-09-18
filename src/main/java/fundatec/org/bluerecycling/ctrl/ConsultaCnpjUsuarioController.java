65cc8a64778a655bf9a60833d0a4508c0d4b32f8
Marcela P Proenca <marcelaproencaeng@gmail.com>
2023-09-18 00:06:26 -0300

    Ajustando sgbd  para mysql

--- src/main/java/fundatec/org/bluerecycling/ctrl/ConsultaCnpjUsuarioController.java
+++ src/main/java/fundatec/org/bluerecycling/ctrl/ConsultaCnpjUsuarioController.java
@@ -1,23 +1,23 @@
-package fundatec.org.bluerecycling.ctrl;
-
-import fundatec.org.bluerecycling.dto.ConsultaCepUsuarioRequestDTO;
-import fundatec.org.bluerecycling.dto.ConsultaCnpjUsuarioRequestDTO;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-@RestController
-@RequestMapping("consulta-cnpjusuarios")
-@CrossOrigin(origins = "*")
-public class ConsultaCnpjUsuarioController {
-    @GetMapping("/{cnpj}")
-    public ConsultaCnpjUsuarioRequestDTO consultaCnpjUsuario(@PathVariable("cnpj") String cnpj) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<ConsultaCnpjUsuarioRequestDTO> resp =
-                restTemplate.getForEntity(
-                        String.format(
-                                "https://www.receitaws.com.br/v1/%s/json", cnpj),
-                        ConsultaCnpjUsuarioRequestDTO.class);
-        return resp.getBody();
-    }
-}
+//package fundatec.org.bluerecycling.ctrl;
+//
+//import fundatec.org.bluerecycling.dto.ConsultaCepUsuarioRequestDTO;
+//import fundatec.org.bluerecycling.dto.ConsultaCnpjUsuarioRequestDTO;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.client.RestTemplate;
+//
+//@RestController
+//@RequestMapping("consulta-cnpjusuarios")
+//@CrossOrigin(origins = "*")
+//public class ConsultaCnpjUsuarioController {
+//    @GetMapping("/{cnpj}")
+//    public ConsultaCnpjUsuarioRequestDTO consultaCnpjUsuario(@PathVariable("cnpj") String cnpj) {
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<ConsultaCnpjUsuarioRequestDTO> resp =
+//                restTemplate.getForEntity(
+//                        String.format(
+//                                "https://www.receitaws.com.br/v1/%s/json", cnpj),
+//                        ConsultaCnpjUsuarioRequestDTO.class);
+//        return resp.getBody();
+//    }
+//}
